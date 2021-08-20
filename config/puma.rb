3290543165b9050349419cef6090232ee9128299
Edison Garcia <edisga@microsoft.com>
2021-08-20 13:54:09 +0000

    Adding performance Puma's values

--- config/puma.rb
+++ config/puma.rb
@@ -22,7 +22,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 #
 
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+workers ENV.fetch("WEB_CONCURRENCY") { 10 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
